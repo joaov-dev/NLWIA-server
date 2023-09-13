064b0bda0385ce172c966d438ccb1817fdf5b961
Joao Victor Santos <jhony2512@outlook.com>
2023-09-12 23:25:56 -0300

    Initial Commit - day2

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import { prisma } from './lib/prisma';
+import { fastify } from 'fastify';
+import { getAllPromptsRoutes } from './routes/get-all-prompts';
+import { uploadVideoRoute } from './routes/upload-video';
+
+const app = fastify();
+
+app.register(getAllPromptsRoutes)
+app.register(uploadVideoRoute)
+
+app.listen({
+	port: 3333
+}).then(() => {
+	console.log("listening on port: 3333")
+})
